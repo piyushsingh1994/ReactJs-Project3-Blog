be5245d78ce140ac80c0cd3b2069e0442e82463e
Piyush Singh <piyushkumar194@gmail.com>
2018-11-24 23:32:28 +0530

    Updates

---
+++ precache-manifest.de40e85307313a20893943ef88ed75e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e547005550da02397cb1",
+    "url": "/ReactJs-Project3-Blog/static/css/main.80cbb421.chunk.css"
+  },
+  {
+    "revision": "e547005550da02397cb1",
+    "url": "/ReactJs-Project3-Blog/static/js/main.e5470055.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/ReactJs-Project3-Blog/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "40035ce7975a155677a4",
+    "url": "/ReactJs-Project3-Blog/static/js/runtime~main.40035ce7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactJs-Project3-Blog/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a3f4882cd9c56f4d81ed4f11f57a4a87",
+    "url": "/ReactJs-Project3-Blog/index.html"
+  }
+];
